face534d2508016b1c64c110e5679096996f4346
Jackson <jacksonlee21@comcast.net>
2020-02-27 14:28:36 -0800

    Basic movement is done

---
+++ DeliveryGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Windows.Input;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 1;
+
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.position -= transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.position -= transform.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+
+    }
+}
+
