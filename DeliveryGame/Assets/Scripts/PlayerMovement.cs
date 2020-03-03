7adb2cfb0d3791e87ee65d85372fc357ccbe2f0d
Daniel <danielmholland1@gmail.com>
2020-03-03 10:13:23 -0800

    started to work on making movment using forces

--- DeliveryGame/Assets/Scripts/PlayerMovement.cs
+++ DeliveryGame/Assets/Scripts/PlayerMovement.cs
@@ -5,32 +5,51 @@ using System.Windows.Input;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed;
-    float jumpPower;
+    public float acc = 15f;
+    public float jumpPower = 300f;
+    public float CofKinFric = 1f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+      rb = GetComponent<Rigidbody>();
+    }
 
     void Update()
     {
         movement();
         Jump();
+        Debug.Log(rb.velocity);
     }
 
     void movement()
     {
         if (Input.GetKey("w"))
         {
-            transform.position += transform.forward * speed * Time.deltaTime;
+            //transform.position += transform.forward * acc * Time.deltaTime;
+            rb.AddForce(transform.forward * acc);
         }
         if (Input.GetKey("s"))
         {
-            transform.position -= transform.forward * speed * Time.deltaTime;
+            //transform.position -= transform.forward * acc * Time.deltaTime;
+            rb.AddForce(-transform.forward * acc);
         }
         if (Input.GetKey("a"))
         {
-            transform.position -= transform.right * speed * Time.deltaTime;
+            //transform.position -= transform.right * acc * Time.deltaTime;
+            rb.AddForce(-transform.right * acc);
         }
         if (Input.GetKey("d"))
         {
-            transform.position += transform.right * speed * Time.deltaTime;
+            //transform.position += transform.right * acc * Time.deltaTime;
+            rb.AddForce(transform.right * acc);
+        }
+
+        //im trying to code friction cus i dumb
+        if(Mathf.Abs(rb.velocity.x) > 0f || Mathf.Abs(rb.velocity.z) > 0f)
+        {
+            rb.AddForce(CofKinFric * rb.mass * -Mathf.Sign(rb.velocity.x) * transform.right);
+            rb.AddForce(CofKinFric * rb.mass * -Mathf.Sign(rb.velocity.z) * transform.forward);
         }
     }
 
@@ -38,7 +57,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            GetComponent<Rigidbody>().AddForce(Vector3.up * 300);
+            rb.AddForce(Vector3.up * jumpPower);
         }
     }
 }
